7a2685aac7087abac81cd8195c638f9cd302dec6
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-06-26 16:04:16 +0530

    Updates

---
+++ precache-manifest.11a1423a042fff5c0dfad005ca9a302c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3bc22be2f8c19cf380eb512181c30e",
+    "url": "/ManojSilag/fruit_shop/index.html"
+  },
+  {
+    "revision": "cf82fbe44dcd98da8328",
+    "url": "/ManojSilag/fruit_shop/static/css/main.20be8ec5.chunk.css"
+  },
+  {
+    "revision": "0fc4b2a9b2bc6730658d",
+    "url": "/ManojSilag/fruit_shop/static/js/2.c799a965.chunk.js"
+  },
+  {
+    "revision": "cf82fbe44dcd98da8328",
+    "url": "/ManojSilag/fruit_shop/static/js/main.58b0c63d.chunk.js"
+  },
+  {
+    "revision": "298270f834dc959ea8ef",
+    "url": "/ManojSilag/fruit_shop/static/js/runtime~main.120fd299.js"
+  }
+]);
